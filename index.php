6c65697793fe39e91bc63dc02bff7e420fbf3863
Ryamos <ramosryanceasar@gmail.com>
2023-11-13 20:41:49 +0800

    Image of each button

--- index.php
+++ index.php
@@ -3,72 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-<link rel="stylesheet" href="style.css">
+    <title>Sustainable Development Goals</title>
+<link rel="stylesheet" href="sdg-layout.css">
+<link rel="stylesheet" href="sdg-design.css">
 </head>
 <body>
-  <header>
 
+<!--1-->
+<header>
+<button class="nav-icon" id="nav">
+  <img src="https://th.bing.com/th/id/R.bbb63fef2299fb2221c7de54f6dc0d74?rik=IW%2bOZITXRJjZPA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_422593.png&ehk=oG3N4HbMA13V51PirqxjhKZXSpzlascIVHJXBZUysoU%3d&risl=&pid=ImgRaw&r=0" alt="Image" width="30px" height="30px">
+</button>
+<input type="text" class="search" placeholder="Search">
   </header>
-  <div class="about-sdg">
 
+<!--2-->
+  <nav>
+    <div class="logo-container">
+  <img src="IMG/logo.png" class='logo' alt="Image" width="auto" height="auto">
+  </div>
+ <div class="slideshow-opacity">
+   <div class="opacity1"></div>
+<div class="opacity2"></div>
+</div>
+  </nav>
+ <!--3-->
+
+  <div class="about-sdg">
   </div>
 
+
+<!--4-->
+
+
+<center>
   <div class="sdg-container">
-  <button class="sdg" id="sdg1" name="sdg1" style=" background-color: darkorange;"><h1>1 NO POVERTY</h1></button>
-<iframe id="iframe1" src="sdg/sdg1.php" frameborder="1"></iframe>
+    <div class="first-layer">
+    <a href="sdg/sdg1.php"> <button class="sdg" id="sdg1" name="sdg1" style=" background-color: darkorange;"></button></a>
+    <a href="sdg/sdg2.php"><button class="sdg" id="sdg2" name="sdg2" style=" background-color: darkred;" ></button></a>
+    <a href="sdg/sdg3.php"><button class="sdg" id="sdg3" name="sdg3" style=" background-color: lightgreen;"></button></a>
+    <a href="sdg/sdg4.php"><button class="sdg" id="sdg4" name="sdg4" style=" background-color: lightblue;"></button></a>
+    <a href="sdg/sdg5.php"><button class="sdg" id="sdg5" name="sdg5" style=" background-color: darkyellow;"></button></a>
+    <a href="sdg/sdg6.php"><button class="sdg" id="sdg6" name="sdg6" style=" background-color: darkblue;"></button></a>
+    <a href="sdg/sdg7.php"><button class="sdg" id="sdg7" name="sdg7" style=" background-color: darkorange;"></button></a>
+</div>
 
-<button class="sdg" id="sdg2" name="sdg2" style=" background-color: darkred;" ><h1>2 ZERO HUNGER</h1></button>
-<iframe id="iframe2" src="sdg/sdg2.php" frameborder="1"></iframe>
+<div class="second-layer">
+<a href="sdg/sdg8.php"><button class="sdg" id="sdg8" name="sdg8" style=" background-color:darkbrown;"></button></a>
+<a href="sdg/sdg9.php"><button class="sdg" id="sdg9" name="sdg9" style=" background-color: darkblue;"></button></a>
+<a href="sdg/sdg10.php"><button class="sdg" id="sdg10" name="sdg10" style=" background-color: darkgreen;"></button></a>
+<a href="sdg/sdg11.php"><button class="sdg" id="sdg11" name="sdg11" style=" background-color: lightblue;"></button></a>
+<a href="sdg/sdg12.php"><button class="sdg" id="sdg12" name="sdg12" style=" background-color: lightgreen;"></button></a>
+<a href="sdg/sdg13.php"><button class="sdg" id="sdg13" name="sdg13" style=" background-color: darkgreen;"></button></a>
+</div>
 
-<button class="sdg" id="sdg3" name="sdg3" style=" background-color: lightgreen;"><h1>3 GOOD HEALTH AND WELL-BEING</h1></button>
-<iframe id="iframe3" src="sdg/sdg3.php" frameborder="1"></iframe>
+<div class="third-layer">
+<a href="sdg/sdg14.php"><button class="sdg" id="sdg14" name="sdg14" style=" background-color: lightblue;"></button></a>
+<a href="sdg/sdg15.php"><button class="sdg" id="sdg15" name="sdg15" style=" background-color: lightgreen;"></button></a>
+<a href="sdg/sdg16.php"><button class="sdg" id="sdg16" name="sdg16" style=" background-color: darkgreen;"></button></a>
+<a href="sdg/sdg17.php"><button class="sdg" id="sdg17" name="sdg17" style=" background-color: lightblue;"></button></a>
+<button class="sdg" id="sdg18" name="sdg18" style=" background-color: lightblue;"><h1>SEE MORE</h1></button>
 
-<button class="sdg" id="sdg4" name="sdg4" style=" background-color: lightblue;"><h1>4 QUALITY EDUCATION</h1></button>
-<iframe id="iframe4" src="sdg/sdg4.php" frameborder="1"></iframe>
+</div>
+</center>
 
-<button class="sdg" id="sdg5" name="sdg5" style=" background-color: darkyellow;"><h1>5 GENDER EQUALITY</h1></button>
-<iframe id="iframe5" src="sdg/sdg5.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg6" name="sdg6" style=" background-color: darkblue;"><h1>6 CLEAN WATER AND SANITATION<h1></button>
-<iframe id="iframe6" src="sdg/sdg6.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg7" name="sdg7" style=" background-color: darkorange;"><h1>7 AFFORDABLE AND CLEAN ENERGY</h1></button>
-<iframe id="iframe7" src="sdg/sdg7.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg8" name="sdg8" style=" background-color:darkbrown;"><h1>8 DECENT WORK AND ECONOMIC GROWTH</h1></button>
-<iframe id="iframe8" src="sdg/sdg8.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg9" name="sdg9" style=" background-color: darkblue;"><h1>9 INDUSTRY INNOVATION AND INFRASTRUCTURE</h1></button>
-<iframe id="iframe9" src="sdg/sdg9.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg10" name="sdg10" style=" background-color: darkgreen;"><h1>10 REDUCED INEQUALITIES</h1></button>
-<iframe id="iframe10" src="sdg/sdg10.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg11" name="sdg11" style=" background-color: lightblue;"><h1>11 SUSTAINABLE CITIES AND COMMUNITIES</h1></button>
-<iframe id="iframe11" src="sdg/sdg11.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg12" name="sdg12" style=" background-color: lightgreen;"><h1>12 RESPONSIBLE CONSUMPTION AND PRODUCTION</h1></button>
-<iframe id="iframe12" src="sdg/sdg12.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg13" name="sdg13" style=" background-color: darkgreen;"><h1>13 CLIMATE ACTION</h1></button>
-<iframe id="iframe13" src="sdg/sdg13.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg14" name="sdg14" style=" background-color: lightblue;"><h1>14 LIFE BELOW WATER</h1></button>
-<iframe id="iframe14" src="sdg/sdg14.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg15" name="sdg15" style=" background-color: lightgreen;"><h1>15 LIFE ON LAND</h1></button>
-<iframe id="iframe15" src="sdg/sdg15.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg16" name="sdg16" style=" background-color: darkgreen;"><h1>16 PEACE, JUSTICE AND STRONG INSTITUTIONS</h1></button>
-<iframe id="iframe16" src="sdg/sdg16.php" frameborder="1"></iframe>
-
-<button class="sdg" id="sdg17" name="sdg17" style=" background-color: lightblue;"><h1>17 PARTNERSHIPS FOR GOALS</h1></button>
-<iframe id="iframe17" src="sdg/sdg17.php" frameborder="1"></iframe>
-
-
-
-<script src="script.js"></script>
 </div>
 
 </body>
